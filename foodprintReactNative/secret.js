1cff617d46b95277f74b72fd73d710bca32319ec
moneychaudhary5 <moneychaudhary5@gmail.com>
2018-05-29 10:17:58 +0530

    secret js

---
+++ foodprintReactNative/secret.js
@@ -0,0 +1,21 @@
+export const clarifaiKeys = {
+  apikey : 'b460f6ab71ef4d499f89deb7b1010bef'
+}
+
+export const AWSOptions = {
+  bucket: "foodify-anupam",
+  region: "AKIAJDU226L2DFEJLZEQ",
+  accessKey: "ziDqc+KDCRVPuP6TIvfxTEro1XK0vLKiXUjmSuCk",
+  secretKey: "us-east-1",
+  successActionStatus: 201
+}
+
+export const nutritionixConfig = {
+  headers: {
+    'X-app-id': "d87c5c39",
+    'X-app-key': "d107f8e2df07ca01ea623490f01a313b",
+    'Content-Type': 'application/json'
+  }
+};
+
+export const nutritionixURL = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
